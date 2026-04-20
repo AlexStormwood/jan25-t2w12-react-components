a6d734d96dd162e1bb5b6c84e0f4430d27ca1b73
Alex <alex.stormwood@bigfootds.com>
2026-04-20 19:59:29 +1000

    feat: custom react component

---
+++ src/components/FavouritePokemonList.jsx
@@ -0,0 +1,23 @@
+
+
+let pokemonNames = [
+	"Pikachu",
+	"Mewtwo",
+	"Charizard",
+	"Briochien"
+];
+
+export default function FavouritePokemonList(){
+	
+	return (
+		<div>
+			<h1>Favourite Pokemon List</h1>
+			{/* array.forEach is for iterating over an array */}
+			{/* array.map is for creating a new array based on iterating over another array */}
+			{/* so we make an array of h2 elements based on pokemonNames! */}
+			{pokemonNames.map((name, index) => {
+				return <h2 key={`${name}-${index}`} >{name}</h2>
+			})}
+		</div>
+	)
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles/index.css'
+import App from './components/App.jsx'
+import FavouritePokemonList from './components/FavouritePokemonList.jsx'
+
+// Find an element on the current webpage that we can latch on to
+// let reactRootElement = document.getElementById("root");
+
+// This would replace the ENTIRE body with React, which isn't always great
+// reactRoot = document.body;
+
+// Initialise React inside the chosen HTML element
+// let readyReactRoot = createRoot(reactRootElement);
+
+// Tell React to start rendering and running
+// readyReactRoot.render(<div>Hello, world!</div>)
 
 createRoot(document.getElementById('root')).render(
+  // React app starts now!!!
   <StrictMode>
+    <FavouritePokemonList />
     <App />
   </StrictMode>,
 )
