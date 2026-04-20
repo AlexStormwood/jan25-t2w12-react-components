86a5b2c5ceca43c8b5daf1bfc525f788b8fa0e88
Alex <alex.stormwood@bigfootds.com>
2026-04-20 20:50:47 +1000

    fix: conditional rendering to remove briochien

--- src/components/PokemonNameCard.jsx
+++ src/components/PokemonNameCard.jsx
@@ -5,9 +5,28 @@
 // So we just assume that it has value within this file!
 export default function PokemonNameCard({pokemonName}){
 
-	return(
-		<h2>
-			{pokemonName}
-		</h2>
-	)
+	// conditional rendering via if conditions
+	if (pokemonName.toString().toLocaleLowerCase() == "briochien"){
+		return null;
+	} else {
+		return(
+			<h2>
+				{pokemonName}
+			</h2>
+		)
+	}
+
+	// short-circuit and/or ternary logic to conditionally render when
+	// already inside the return 
+	// return (
+	// 	<>
+	// 		{pokemonName.toString().toLocaleLowerCase() != "briochien" ? 
+	// 		<h2>
+	// 			{pokemonName}
+	// 		</h2> 
+	// 		: 
+	// 		null
+	// 		}
+	// 	</>
+	// )
 }
